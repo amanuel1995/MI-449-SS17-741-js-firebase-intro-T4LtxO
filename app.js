e80c7952ccdb7634aceb702e92226cc536b49208
Amanuel <amaniye1@live.com>
2017-04-18 17:17:28 -0400

    Initial Setup with Firebase and base files

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCQfxc7nWuy-8JrzLnoK1oSUJynzVHY-XQ",
+   authDomain: "joke-a-tron-9000-9c753.firebaseapp.com",
+   databaseURL: "https://joke-a-tron-9000-9c753.firebaseio.com",
+   projectId: "joke-a-tron-9000-9c753",
+   storageBucket: "joke-a-tron-9000-9c753.appspot.com",
+   messagingSenderId: "610945650935"
+ };
+ firebase.initializeApp(config);
